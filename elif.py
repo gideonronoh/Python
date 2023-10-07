8934f8d3f82c51ce4ed5b32f73c78635eae18c2d
Gideon Rono <145061874+28ron@users.noreply.github.com>
2023-10-07 13:29:14 +0300

    elif.py

---
+++ elif.py
@@ -0,0 +1,29 @@
+#AGE
+
+age =int(input("Enter your age"))
+if age <= 18:
+   print("You are not eligible")
+else:
+   print("You are eligible")
+
+#NUMBERS
+
+number = int(input("Enter a number"))
+if number % 2==0:
+    print ("Even Number")
+else:
+    print('Odd')
+#GRADE
+grade = input("enter your grade(A,B,C,D,E,F:")
+if grade == "A":
+    print("Wonderful")
+elif grade == "B":
+    print("You have the potential")
+elif grade == "C":
+    print("improve")
+elif grade == "D":
+    print("Terrble ")
+elif grade == "E":
+    print("gaaaangggg!!")
+else:
+    print("invalid")                    
