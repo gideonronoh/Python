1ddb463a696bc38f2c97482eae24a13807ac6bf0
Gideon Rono <145061874+28ron@users.noreply.github.com>
2023-10-09 17:34:19 +0300

    loop2.py

---
+++ loop2.py
@@ -0,0 +1,24 @@
+#count = 9
+#while count < 5:
+ #   print("Hello")
+  #  count= count+1
+
+  
+#for j in range(5):
+     # for i in range(j + 1):
+      #  print("*", end=" ")
+       # print()
+        
+        #n = 5 Change this value to adjust the size of the upside-down triangle
+#n = 5
+#for i in range(n, 0, -1):
+ #  for j in range(n,- i):
+  #      print(" ", end="")
+#for k in range(2 * i - 1):
+ #       print("*", end="")
+  #      print()
+
+num1= 10000
+num2 = 20000
+ans = num1*num2
+print(f'{ans:_}') 
