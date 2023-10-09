dbd25215e4749ebda20d11d05e4c1818758b272a
Gideon Rono <145061874+28ron@users.noreply.github.com>
2023-10-09 17:32:36 +0300

    index.py

---
+++ index.py
@@ -0,0 +1,13 @@
+#name = "bro code"
+#if allchar.islower()):
+  #name = name.capitalize()
+  #print(name)           
+name = "bro code!"
+#if all(char.islower() for char in name[:5]):
+    #name = name.capitalize()
+first_name=name[:8].upper()
+last_name=name[4:].lower()
+last_character= name[-1]
+print(first_name)
+print(last_name)
+print(last_character)
