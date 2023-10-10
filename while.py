9ba7f89d2a5f54945462dfa5a3a175b5609f6260
Gideon Rono <145061874+28ron@users.noreply.github.com>
2023-10-10 16:27:30 +0300

    while.py

---
+++ while.py
@@ -0,0 +1,16 @@
+
+#num = 0
+#while (num < 90):
+#	print(num)
+#	num = num + 1 #update the value of num by 1
+#num has been incrememnted therefore it stops at 17
+num = 1
+while num < 17:
+    print(num)
+   num = num + 1
+
+#num has not been incremented therefore it goes as an infinite loop
+
+num = 1
+while(num<12):
+ print(num)
