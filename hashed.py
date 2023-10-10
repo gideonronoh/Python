250af2549dac9c04bd47fa88f77a6b84ce6ba1b0
Gideon Rono <145061874+28ron@users.noreply.github.com>
2023-10-10 15:44:40 +0300

    hashed.py

---
+++ hashed.py
@@ -0,0 +1,45 @@
+#count = 9
+#while count < 5:
+ #   print("Hello")
+  #  count= count+1
+
+  
+#for j in range(5):
+     # for i in range(j + 1):
+      #  print("*", end=" ")
+       # print()
+        
+        #n = 5 Change this value to adjust the size of the upside-down triangle
+#n = 5
+#for i in range(n, 0, -1):
+ #  for j in range(n,- i):
+  #      print(" ", end="")
+#for k in range(2 * i - 1):
+ #       print("*", end="")
+  #      print()
+
+#num1= 10000
+#num2 = 20000
+#ans = num1*num2
+#print(f'{ans:_}') 
+#num1 = int(input("enter first number"))
+#num2 = int(input("Enter second number"))
+#num3 = num1+num2
+#print("Addition is:",num3)
+
+#x= 7
+#y = 8
+#print(("x>y is ,x>y"))
+#if 5 ==6:
+ # print("This is true")
+#elif 4==5:
+ # print("Not really")
+#elif 7==6:
+ # print("Can you stop?")  
+#else:
+  #print("Biggest cap")
+
+#for num in range (1,101):
+  #  print(num)
+#for num in range(1,10,2):
+  #print(num)
