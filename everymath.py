bdc369d5dcbbec435b13c4c77395489dfb842e63
Gideon Rono <145061874+28ron@users.noreply.github.com>
2023-10-11 14:20:03 +0300

    everymath.py

---
+++ everymath.py
@@ -0,0 +1,33 @@
+import math
+a = math.floor(4.3)
+b = math.floor(10.9)
+print(a)
+print(b)
+
+a = math.ceil(4.3)
+b = math.ceil(10.9)
+print(a)
+print(b)
+ 
+a = math.fabs(10)
+b = math.fabs(-10)
+print(a)
+print(b)
+
+import math 
+a = math.log(10)  
+b = math.log(10,2)  
+print (a) 
+print (b)
+
+import math 
+a = math.sqrt(9)  
+b = math.sqrt(16)  
+print (a) 
+print (b)
+
+import math
+a = math.sqrt(16)
+b = math.sqrt(25)
+print(a)
+print(b)
