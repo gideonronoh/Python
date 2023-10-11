a3386527a9a0ec8708ddf3a4243b3b9962cf01f9
Gideon Rono <145061874+28ron@users.noreply.github.com>
2023-10-11 18:14:37 +0300

    string.py

---
+++ string.py
@@ -0,0 +1,16 @@
+str = "here I Am."
+str= str.capitalize()
+print(str)
+
+str = "Here i am"
+str = str.count("e")
+print(str)
+
+str = "here I am"
+str = str.find("h")
+print(str)
+
+str = " "
+iter = "I","am","awesome"
+a = str.join(iter)
+print(a)
