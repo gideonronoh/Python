05b5a4b809323801a29b5a1180eed1f477e4701d
Gideon Rono <145061874+28ron@users.noreply.github.com>
2023-10-12 11:37:31 +0300

    listmod.py

---
+++ listmod.py
@@ -0,0 +1,22 @@
+list2 = [23, 45, 56, 78]
+list2[3] = [44]
+list 1 = [2,4,5,6,72]
+list 2=[4,"hi",6,"me"]
+
+#to access items on  a list
+list 1 = [2,4,5,6,7]
+a = list 1[1:4]
+print(a)
+
+#to append items on a list
+list 2 = [3,4,5,6,7]
+list 2.append(150)
+
+#to delete items on a list
+list 1 = [3,4,5,6,]
+del list 1[3]
+#we can also use the remove function for the same purpose
+
+list[1,2,3,4,5]
+list.remove[5]
+print(list1)
