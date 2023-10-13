7463fb1fb0f3726daffe09f74a80177fb39691e1
Gideon Rono <145061874+28ron@users.noreply.github.com>
2023-10-13 13:21:31 +0300

    Birthday.py

---
+++ birthday.py
@@ -0,0 +1,8 @@
+
+def wish_birthday():
+    recipient_name = "Kiongoss"
+    sender_name = "Simimi"
+    birthday_message = f"Happy birthday {recipient_name}, from {sender_name}!"
+    print(birthday_message)
+if __name__ == "__main__":
+    wish_birthday()
