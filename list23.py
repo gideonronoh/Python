18b07c957c10727aa88cb7102d39ed5fc5443e01
Gideon Rono <145061874+28ron@users.noreply.github.com>
2023-11-08 18:07:03 +0300

    list23.py

---
+++ list23.py
@@ -0,0 +1,12 @@
+#mylists = ["banana, cherry, blueberry"]
+#print(mylists)
+#if "banana" in mylists:
+ #   print("yes")
+else:
+   print ("no")
+
+mylists = ["banana,cherry,blueberry"]
+print(mylists)
+print(len(mylists))
+mylists.insert(1, "blueberry")
+print(mylists)
