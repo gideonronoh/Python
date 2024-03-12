bbfe1991be26a7298cd07dd140b239f61fb16548
Gideon Rono <145061874+gideonronoh@users.noreply.github.com>
2024-03-12 21:50:53 +0300

    Tuples

---
+++ Tuples.py
@@ -0,0 +1,15 @@
+tup1 = ('harry','ron');
+tup2 = (1,2,3);
+tup3 = (1,2,3);
+ list1 = ['a','b','c']
+ print"Comparison of tup1 $ tup2:",
+ cmp(tup1,tup2)
+ print"Comparison of tup2 $ tup3:",
+ cmp(tup2,tup3)
+
+ print'Lenght of tup2:',len(tup2)
+ print 'Max of tup2:',max(tup2)
+ print'Min of tup2:',min(tup2)
+
+ tup4 = tuple(list1)
+ print "Converted tuple from list:",tup4 
